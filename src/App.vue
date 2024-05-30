d3c46650e47be9b5e79f584fad4e8202e655294e
吴舒先 <2893251844@qq.com>
2024-05-30 17:45:05 +0800

    测试视图能否正常跳转--在App中使用导航router-link

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <nav>
+      <button>
+        <router-link to="/main">主页面</router-link>
+      </button>
+    </nav>
+    <router-view/>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>>
 
 <style>
 #app {
